e1afd5c937e178f678df70a0acc495975cd4a197
mo-kasiri <mkgh7030@gmail.com>
2023-12-13 01:00:36 +0330

    first commit - main scene

---
+++ cache/js/_editor_index.js
@@ -0,0 +1,6 @@
+import * as __wonderlandengine_components from '@wonderlandengine/components';
+_registerEditor(__wonderlandengine_components);
+import * as _C__Users_mkgh5_Documents_WonderlandEngine_3d_labels_vr_js_button_js from 'C:/Users/mkgh5/Documents/WonderlandEngine/3d-labels-vr/js/button.js';
+_registerEditor(_C__Users_mkgh5_Documents_WonderlandEngine_3d_labels_vr_js_button_js);
+import * as _C__Users_mkgh5_Documents_WonderlandEngine_3d_labels_vr_pp_index_js from 'C:/Users/mkgh5/Documents/WonderlandEngine/3d-labels-vr/pp/index.js';
+_registerEditor(_C__Users_mkgh5_Documents_WonderlandEngine_3d_labels_vr_pp_index_js);
